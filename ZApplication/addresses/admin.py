edd29b991a2d15c6e98c39605f8083f791e8d9f0
siberian-cat <siberian-cat@yandex.ru>
2024-01-24 15:07:42 +0700

    Added all models in Addresses

--- ZApplication/addresses/admin.py
+++ ZApplication/addresses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from addresses.models import StreetType, Street, Locality, Address
+
+admin.site.register(StreetType)
+admin.site.register(Street)
+admin.site.register(Locality)
+admin.site.register(Address)
--- ZApplication/addresses/models.py
+++ ZApplication/addresses/models.py
@@ -4,14 +4,23 @@ from django.db import models
 class StreetType(models.Model):
     streettype_name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return f"{self.streettype_name}"
+
 
 class Street(models.Model):
     street_name = models.CharField(max_length=255)
     streettype = models.ForeignKey(StreetType, related_name="streets", on_delete=models.PROTECT)
 
+    def __str__(self):
+        return f"{self.street_name} {self.streettype.streettype_name}"
+
 
 class Locality(models.Model):
     locality_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.locality_name}"
 
 
 class Address(models.Model):
@@ -19,3 +28,5 @@ class Address(models.Model):
     locality = models.ForeignKey(Locality, related_name="addresses", on_delete=models.PROTECT)
     street = models.ForeignKey(Street, related_name="addresses", on_delete=models.PROTECT)
 
+    def __str__(self):
+        return f"{self.locality.locality_name}, {self.street.street_name} {self.street.streettype.streettype_name}, {self.build_num}"
