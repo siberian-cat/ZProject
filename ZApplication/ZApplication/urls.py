9cf54b045f926e8c264614df69e6436e46e74bc0
siberian-cat <siberian-cat@yandex.ru>
2024-01-29 05:50:13 +0700

    02. Начало работы с front-end

--- ZApplication/ZApplication/urls.py
+++ ZApplication/ZApplication/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from addresses.views import index
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('addresses/', include('addresses.urls')),
 ]
